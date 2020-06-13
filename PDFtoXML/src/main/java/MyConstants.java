bf68e13a25aa3c0f23946f140d72397d7016ab66
BhavyaCiti <65582884+BhavyaCiti@users.noreply.github.com>
2020-06-13 16:37:07 +0530

    Update MyConstants.java

--- PDFtoXML/src/main/java/MyConstants.java
+++ PDFtoXML/src/main/java/MyConstants.java
@@ -2,8 +2,8 @@
 public class MyConstants {
 
 	public static final float maxGapBetweenTwoRows = 10;
-	public static final float colonPercentage = 0.15;
-	public static final float boldPercentage = 0.30;
+	public static final double colonPercentage = 0.15;
+	public static final double boldPercentage = 0.30;
 	public static final int maxGapBetweenBoxes = 20;
 	public static final float mergeLinesDistance = 2;
 }
