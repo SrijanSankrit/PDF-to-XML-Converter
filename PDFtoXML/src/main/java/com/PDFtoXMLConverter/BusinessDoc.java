6498aa95e3d3e82af398e299d12dff8012741e1c
BhavyaCiti <65582884+BhavyaCiti@users.noreply.github.com>
2020-06-16 18:48:04 +0530

    Update BusinessDoc.java

--- PDFtoXML/src/main/java/com/PDFtoXMLConverter/BusinessDoc.java
+++ PDFtoXML/src/main/java/com/PDFtoXMLConverter/BusinessDoc.java
@@ -1,70 +1,28 @@
 package com.PDFtoXMLConverter;
 import java.util.*;
+import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 
 import java.io.IOException;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
 
-/**
- * General class of documents that an organization can have
- * @author BHAVYA SHARMA
- *  
- */
 
+//General class of documents that an organization can have
+//Holds all the lines, rectangles and boxes present in the doc as fields
 public class BusinessDoc {
-
-	private PDPage docObj;				
-	private List<PDFLine> docLines;								//Lines in the page
-	private List<Box> docBoxes;									//Boxes in the page
-	private List<MyPDRectangle> docRectangles;					//Rectangles in the page
-
 	
-	/**
-	 * Function to create XML file of the document
-	 */
-	public void createXML(XMLCreator xmlCreator, ArrayList<Integer> pages, int pageNo) throws Exception {
-
+	public PDPage docObj;
+	public List<PDFLine> docLines;	
+	public List<Box> docBoxes ;
+	public List<MyPDRectangle> docRectangles ;
+	public void createXML(XMLCreator xmlCreator, ArrayList<Integer> pages,int pageNo) throws Exception
+	{
+		
+		
 	}
-
-	/**
-	 * Function to extract data in given page
-	 */
-	public void extract(int pageNo) throws IOException {
-
-	}
-
-	/**
-	 * Getters and setters
-	 */
-	
-	public PDPage getDocObj() {
-		return docObj;
-	}
-
-	public void setDocObj(PDPage docObj) {
-		this.docObj = docObj;
-	}
-
-	public List<PDFLine> getDocLines() {
-		return docLines;
-	}
-
-	public void setDocLines(List<PDFLine> docLines) {
-		this.docLines = docLines;
-	}
-
-	public List<Box> getDocBoxes() {
-		return docBoxes;
-	}
-
-	public void setDocBoxes(List<Box> docBoxes) {
-		this.docBoxes = docBoxes;
-	}
-
-	public List<MyPDRectangle> getDocRectangles() {
-		return docRectangles;
-	}
-
-	public void setDocRectangles(List<MyPDRectangle> docRectangles) {
-		this.docRectangles = docRectangles;
+	public void extract(int pageNo) throws IOException
+	{
+		
+		
 	}
 }
